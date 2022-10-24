5df0c3cf8efc745c63281a2784a2e5955ab8bb2a
Dimitri Copley <73848767+Bergenheiser@users.noreply.github.com>
2022-10-24 19:16:50 +0200

    import assets

--- src/View/composants/welcome.php
+++ src/View/composants/welcome.php
@@ -5,7 +5,7 @@
       <div class="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
         <div class="group relative">
           <div class="relative h-80 w-full overflow-hidden rounded-lg bg-white group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1">
-            <img src="<?php echo __DIR__."/../../"?> alt="Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug." class="h-full w-full object-cover object-center">
+            <img src="../../../web/assets/electronique.jpg" alt="ELECTRONIQUE BIP BOOOP" class="h-full w-full object-cover object-center">
           </div>
           <h3 class="mt-6 text-sm text-gray-500">
             <a href="#">
@@ -18,7 +18,7 @@
 
         <div class="group relative">
           <div class="relative h-80 w-full overflow-hidden rounded-lg bg-white group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1">
-            <img src="https://tailwindui.com/img/ecommerce-images/home-page-02-edition-02.jpg" alt="Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant." class="h-full w-full object-cover object-center">
+            <img src="../../../web/assets/techsupport.jpg" alt="JOHNNY REPARE TOUT" class="h-full w-full object-cover object-center">
           </div>
           <h3 class="mt-6 text-sm text-gray-500">
             <a href="#">
