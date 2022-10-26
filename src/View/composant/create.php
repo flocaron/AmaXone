d5cb62a286ea0cfb768a644dbeb2eac6731b4d19
Dimitri Copley <73848767+Bergenheiser@users.noreply.github.com>
2022-10-26 20:00:11 +0200

    fin front end sur liste create et view pour aujourd'hui

--- src/View/composant/create.php
+++ src/View/composant/create.php
@@ -1,5 +1,5 @@
 <div class="mt-10 sm:mt-0">
-        <div class="mt-5 md:col-span-2 md:mt-0">
+        <div class="mt-5 px-10 md:col-span-2 md:mt-0">
             <form enctype="multipart/form-data" action="frontController.php" method="GET">
                 <div class="overflow-hidden shadow sm:rounded-md">
                     <div class=" flex flex-col gap-6 bg-white px-4 py-5 sm:p-6">
@@ -21,7 +21,7 @@
                             <div>
                                 <label for="about" class="block text-sm font-medium text-gray-700">Description</label>
                                 <div class="mt-1">
-                                    <textarea id="about" name="description" rows="3" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" placeholder="la prochaine vache à lait de la compagnie"></textarea>
+                                    <textarea id="about" name="description" rows="3" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"></textarea>
                                 </div>
                             </div>
                             <label class="block text-sm font-medium text-gray-700">Image Article</label>
--- src/View/view.php
+++ src/View/view.php
@@ -4,21 +4,19 @@
     <meta charset="UTF-8">
     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
     <script defer src="https://unpkg.com/alpinejs@3.2.4/dist/cdn.min.js"></script>
-    <title><?php echo $pagetitle;?></title>
+    <title><?php echo $pagetitle; ?></title>
 </head>
 <body>
 <main>
     <section>
         <?php
-            require __DIR__ . "/{$cheminVueBody}";
+        require __DIR__ . "/{$cheminVueBody}";
         ?>
     </section>
 </main>
 
 <footer>
-    <p id="foot">
-       <h3 class="font-normal hover:font-bold">AmaXone ™ Inc.</h3>
-    </p>
+    <!-- à renseigner-->
 </footer>
 
 </body>
