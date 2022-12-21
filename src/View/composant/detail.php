7a82b3349d5fa82f59f6dfa77f386d175dfe4981
flocaron <floflocaron27@gmail.com>
2022-12-22 00:55:49 +0100

    format page

--- src/View/composant/detail.php
+++ src/View/composant/detail.php
@@ -1,17 +1,17 @@
-<h3> Détail de  <?php echo htmlspecialchars($composant->getId()); ?> : </h3>
+<h3> Détail de <?php echo htmlspecialchars($composant->getId()); ?> : </h3>
 
 <article>
     <?php
-        echo "<p> Libelle = " . htmlspecialchars($composant->getLibelle()) . " </p>\n";
-        echo "<p> Prix = " . htmlspecialchars($composant->getPrix()) . " </p>\n";
-        echo "<p> ImgPath = " . htmlspecialchars($composant->getImgPath()) . " </p>\n";
-        echo "<p> Description = " . htmlspecialchars($composant->getDescription()) . " </p>\n";
-        if ($estAdmin) {
-            echo "<div class='mt-5'>";
-            echo "<p><a href='frontController.php?controller=composant&action=delete&id=" . rawurlencode($composant->getId()) . "' > supprimer </a></p>";
-            echo "<p><a href='frontController.php?controller=composant&action=update&id=" . rawurlencode($composant->getId()) . "' > modifier </a></p>";
-            echo "</div>";
-        }
+    echo "<p> Libelle = " . htmlspecialchars($composant->getLibelle()) . " </p>\n";
+    echo "<p> Prix = " . htmlspecialchars($composant->getPrix()) . " </p>\n";
+    echo "<p> ImgPath = " . htmlspecialchars($composant->getImgPath()) . " </p>\n";
+    echo "<p> Description = " . htmlspecialchars($composant->getDescription()) . " </p>\n";
+    if ($estAdmin) {
+        echo "<div class='mt-5'>" .
+            "<a href='frontController.php?controller=composant&action=delete&id=" . rawurlencode($composant->getId()) . "' > ❌ </a>" .
+            "<a href='frontController.php?controller=composant&action=update&id=" . rawurlencode($composant->getId()) . "' > ✅ </a>" .
+            "</div>";
+    }
     ?>
 
 </article>
