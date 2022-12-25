b6b6ccc43ea591c551dab00d7256142f72b8fc33
Dimitri Copley <73848767+Bergenheiser@users.noreply.github.com>
2022-12-25 08:49:09 +0100

    deployed View detail.php

--- src/View/produit/detail.php
+++ src/View/produit/detail.php
@@ -1,18 +1,27 @@
-<h3> Détail de <?php echo htmlspecialchars($produit->getId()); ?> : </h3>
-
-<article>
-    <?php
-    echo "<p> Libelle = " . htmlspecialchars($produit->getLibelle()) . " </p>\n";
-    echo "<p> Prix = " . htmlspecialchars($produit->getPrix()) . " </p>\n";
-    echo "<p> ImgPath = " . htmlspecialchars($produit->getImgPath()) . " </p>\n";
-    echo "<p> Description = " . htmlspecialchars($produit->getDescription()) . " </p>\n";
-    if ($estAdmin) {
-        echo "<div class='mt-5'>" .
-            "<a href='frontController.php?controller=produit&action=delete&id=" . rawurlencode($produit->getId()) . "' > ❌ </a>" .
-            "<a href='frontController.php?controller=produit&action=update&id=" . rawurlencode($produit->getId()) . "' > ✅ </a>" .
-            "</div>";
-    }
-    ?>
-
-</article>
-
+<div class="flex items-center justify-center w-full">
+    <div class="w-full p-4">
+        <div class="flex flex-col justify-center p-10 bg-white rounded-lg shadow-2xl card">
+            <div class="prod-title">
+                <p class="text-2xl font-bold text-gray-900 uppercase">
+                    <?php echo htmlspecialchars($produit->getLibelle()); ?>
+                </p>
+                <p class="text-sm mt-5 text-gray-400 uppercase">
+                <?php echo htmlspecialchars($produit->getDescription()); ?>
+                </p>
+            </div>
+            <div class="prod-img mt-5">
+                <img src="<?php echo "../assets/images/" . htmlspecialchars($produit->getImgPath()); ?>" class="object-cover object-center w-full"/>
+            </div>
+            <div class="grid gap-10 prod-info">
+                <div class="flex flex-col items-center justify-between text-gray-900 md:flex-row">
+                    <p class="text-xl font-bold">
+                        <?php echo htmlspecialchars($produit->getPrix()) . " €"; ?>
+                    </p>
+                    <button href="frontController.php?controller=produit&action=addPanier&read&id=<?php echo rawurlencode($produit->getId()); ?>" class="px-6 py-2 uppercase transition duration-200 ease-in border-2 border-gray-900 rounded-full hover:bg-gray-800 hover:text-white focus:outline-none">
+                        Ajouter au Panier
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
