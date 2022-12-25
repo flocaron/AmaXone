f3e431b2c6e01b9b3e2b61a5719c54e6bde7cb02
Dimitri Copley <73848767+Bergenheiser@users.noreply.github.com>
2022-12-25 10:16:22 +0100

    View Panier ajout commander

--- src/View/produit/panier.php
+++ src/View/produit/panier.php
@@ -1,3 +1,7 @@
+<?php
+if ($estConnecte) {
+    $commander ="frontController.php?action=passerCommande&controller=commande";
+} //rajouter le message flash ?>
 <div class="relative z-10" aria-labelledby="slide-over-title" role="dialog" aria-modal="true">
     <!--
       Background backdrop, show/hide based on slide-over state.
@@ -114,7 +118,7 @@
                                 </div>
                                 <p class="mt-0.5 text-sm text-gray-500">Frais de ports et taxes incluses.</p>
                                 <div class="mt-6">
-                                    <a href="#"
+                                    <a href="<?php echo $commander ?>"
                                        class="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700">Commander</a>
                                 </div>
                                 <div class="mt-6 flex justify-center text-center text-sm text-gray-500">
