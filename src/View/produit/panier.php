6f038f8f74defba3f268a95b05d5e10a212f46f0
Dimitri Copley <73848767+Bergenheiser@users.noreply.github.com>
2022-12-25 09:57:57 +0100

    Petits edits de la vue list

--- src/View/produit/panier.php
+++ src/View/produit/panier.php
@@ -28,8 +28,7 @@
                         <div class="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
                             <div class="flex-1 overflow-y-auto py-6 px-4 sm:px-6">
                                 <div class="flex items-start justify-between">
-                                    <h2 class="text-lg font-medium text-gray-900" id="slide-over-title">Shopping
-                                        cart</h2>
+                                    <h2 class="text-lg font-medium text-gray-900" id="slide-over-title">Panier d'achats</h2>
                                     <div class="ml-3 flex h-7 items-center">
                                         <button type="button" class="-m-2 p-2 text-gray-400 hover:text-gray-500">
                                             <span class="sr-only">Close panel</span>
@@ -59,7 +58,7 @@
 
                                                 <li class="flex py-6">
                                                     <div class="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
-                                                        <img src="<?php echo "../assets/images/ ". htmlspecialchars($produit->getImgPath()); ?>"
+                                                        <img src="<?php echo "../assets/images/". htmlspecialchars($produit->getImgPath()); ?>"
                                                              alt="Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch."
                                                              class="h-full w-full object-cover object-center">
                                                     </div>
@@ -70,18 +69,18 @@
                                                                 <h3>
                                                                     <a href="#"><?php echo htmlspecialchars($produit->getLibelle()) ?> </a>
                                                                 </h3>
-                                                                <p class="ml-4"><?php echo htmlspecialchars($produit->getPrix()) ?></p>
+                                                                <p class="ml-4"><?php echo htmlspecialchars($produit->getPrix()) ?> €/p.</p>
                                                             </div>
                                                         </div>
                                                         <div class="flex flex-1 items-end justify-between text-sm">
-                                                            <p class="text-gray-500">Qty <?php echo $qte; ?></p>
+                                                            <p class="text-gray-500">x <?php echo $qte; ?></p>
 
                                                             <div class="flex">
                                                                 <a href="frontController.php?action=addPanier&controller=produit&id=<?php echo htmlspecialchars($produit->getId()) ?>">
                                                                     <button type="button"
                                                                             class="font-medium text-indigo-600 hover:text-indigo-500">
 
-                                                                        Add
+                                                                        Ajouter
                                                                     </button>
                                                                 </a>
                                                             </div>
@@ -92,7 +91,7 @@
                                                                     <button type="button"
                                                                             class="font-medium text-indigo-600 hover:text-indigo-500">
 
-                                                                        Remove
+                                                                        Retirer
                                                                     </button>
                                                                 </a>
 
@@ -110,21 +109,21 @@
 
                             <div class="border-t border-gray-200 py-6 px-4 sm:px-6">
                                 <div class="flex justify-between text-base font-medium text-gray-900">
-                                    <p>Subtotal</p>
-                                    <p>$<?php echo $total; ?></p>
+                                    <p>Sous-Total : </p>
+                                    <p><?php echo $total; ?>€</p>
                                 </div>
-                                <p class="mt-0.5 text-sm text-gray-500">Shipping and taxes calculated at checkout.</p>
+                                <p class="mt-0.5 text-sm text-gray-500">Frais de ports et taxes incluses.</p>
                                 <div class="mt-6">
                                     <a href="#"
-                                       class="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700">Checkout</a>
+                                       class="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700">Commander</a>
                                 </div>
                                 <div class="mt-6 flex justify-center text-center text-sm text-gray-500">
                                     <p>
-                                        or
+                                        ou
                                         <a href="frontController.php?action=readAll&controller=produit">
                                             <button type="button"
                                                     class="font-medium text-indigo-600 hover:text-indigo-500">
-                                                Continue Shopping
+                                                Continuer d'explorer
                                                 <span aria-hidden="true"> &rarr;</span>
                                             </button>
                                         </a>
