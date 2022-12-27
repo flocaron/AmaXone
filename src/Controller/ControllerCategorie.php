0c9aaf445351f2d065f26709b1ca2578a880dda9
flocaron <floflocaron27@gmail.com>
2022-12-27 16:38:32 +0100

    add create/update action and veiws for categorie

--- src/Controller/ControllerCategorie.php
+++ src/Controller/ControllerCategorie.php
@@ -4,6 +4,7 @@ namespace App\E_Commerce\Controller;
 
 use App\E_Commerce\Lib\ConnexionUtilisateur;
 use App\E_Commerce\Lib\MessageFlash;
+use App\E_Commerce\Model\DataObject\Categorie;
 use App\E_Commerce\Model\Repository\CategorieRepository;
 
 class ControllerCategorie extends GenericController
@@ -21,7 +22,7 @@ class ControllerCategorie extends GenericController
                 $categorie = (new CategorieRepository())->select($_REQUEST['nom']);
                 if (is_null($categorie)) {
                     MessageFlash::ajouter("warning", "Nom non trouvé !!");
-                    header("Location: frontController.php?action=readAll&controller=produit");
+                    header("Location: frontController.php?action=readAll&controller=categorie");
                 } else {
                     self::afficheVue([
                         'categorie' => $categorie,
@@ -31,7 +32,7 @@ class ControllerCategorie extends GenericController
                 }
             } else {
                 MessageFlash::ajouter("danger", "Nom non renseigné !!");
-                header("Location: frontController.php?action=readAll&controller=produit"); // TODO modif redirection vers vue catalogueCategorie
+                header("Location: frontController.php?action=readAll&controller=categorie"); // TODO modif redirection vers vue catalogueCategorie
             }
         } else {
             MessageFlash::ajouter("danger", "Vous n'etes pas Administrateur !");
@@ -41,26 +42,181 @@ class ControllerCategorie extends GenericController
 
     public static function delete()
     {
-        // TODO: Implement delete() method.
-    }
-
-    public static function create()
-    {
-        // TODO: Implement create() method.
-    }
-
-    public static function created()
-    {
-        // TODO: Implement created() method.
+        if (ConnexionUtilisateur::estAdministrateur()) {
+            if (isset($_REQUEST['nom'])) {
+                if (isset($_REQUEST['verif'])) {
+                    $bool = (new CategorieRepository())->delete($_REQUEST['nom']);
+                    if ($bool) {
+                        MessageFlash::ajouter("success", "Catégorie bien supprimé !");
+                    } else {
+                        MessageFlash::ajouter("warning", "Nom non trouvé !!");
+                    }
+                } else {
+                    MessageFlash::ajouter("info", "Etes-vous sur ? " .
+                        " <a href='frontController.php?action=delete&controller=categorie&nom=" .
+                        rawurlencode($_REQUEST['nom']) . "&verif'> oui </a> " .
+                        " <a href='frontController.php?action=readAll&controller=categorie'> non</a>"
+                    );
+                }
+            } else {
+                MessageFlash::ajouter("danger", "Nom non renseigné !!");
+            }
+            header("Location: frontController.php?action=readAll&controller=categorie");
+        } else {
+            MessageFlash::ajouter("danger", "Vous n'etes pas Administrateur !");
+            header("Location: frontController.php");
+        }
     }
 
     public static function update()
     {
-        // TODO: Implement update() method.
+        if (ConnexionUtilisateur::estAdministrateur()) {
+            if (isset($_REQUEST['nom'])) {
+                $categorie = (new CategorieRepository)->select($_REQUEST['nom']);
+                if (is_null($categorie)) {
+                    MessageFlash::ajouter("warning", "Nom non trouvé !!");
+                    header("Location: frontController.php?action=readAll&controller=categorie");
+                } else {
+                    self::afficheVue([
+                        "categorie" => $categorie,
+                        "action" => "update",
+                        "pagetitle" => "Modifier categorie",
+                        "cheminVueBody" => "categorie/create.php",
+                    ]);
+                }
+            } else {
+                MessageFlash::ajouter("danger", "Nom non renseigné !!");
+                header("Location: frontController.php?action=readAll&controller=categorie");
+            }
+        } else {
+            MessageFlash::ajouter("danger", "Vous n'etes pas Administrateur !");
+            header("Location: frontController.php");
+        }
+    }
+
+    public static function create()
+    {
+        if (ConnexionUtilisateur::estAdministrateur()) {
+            self::afficheVue([
+                "action" => "create",
+                "pagetitle" => "Créer Catégorie",
+                "cheminVueBody" => "categorie/create.php",
+            ]);
+        } else {
+            MessageFlash::ajouter("danger", "Vous n'etes pas Administrateur !");
+            header("Location: frontController.php");
+        }
     }
 
     public static function updated()
     {
-        // TODO: Implement updated() method.
+        if (ConnexionUtilisateur::estAdministrateur()) {
+            if (isset($_REQUEST['nom']) && isset($_REQUEST['description'])) {
+                $categorie = (new CategorieRepository())->select($_REQUEST['nom']);
+                if (!is_null($categorie)) {
+                    $categorie->setDescription($_REQUEST['description']);
+                    if (!empty($_FILES['file-upload']) && is_uploaded_file($_FILES['file-upload']['tmp_name'])) {
+                        $pic_path = __DIR__ . "/../../assets/images/categories/" . $_FILES['file-upload']['name'];
+                        $extension = explode('.', $_FILES['file-upload']['name']);
+                        if (in_array(end($extension), ["jpg", "jpeg", "png"]) && $_FILES['file-upload']['size'] < 10 ** 7) {
+                            if (move_uploaded_file($_FILES['file-upload']['tmp_name'], $pic_path)) {
+                                $categorie->setImgPath($_FILES['file-upload']['name']);
+                            } else {
+                                MessageFlash::ajouter("warning", "Importation de l'image échouée");
+                                self::afficheVue([
+                                    "categorie" => $categorie,
+                                    "action" => "update",
+                                    "pagetitle" => "Modifier categorie",
+                                    "cheminVueBody" => "categorie/create.php",
+                                ]);
+                                exit(1);
+                            }
+                        } else {
+                            MessageFlash::ajouter("warning", "Mauvaise extension ou taille de fichier");
+                            self::afficheVue([
+                                "categorie" => $categorie,
+                                "action" => "update",
+                                "pagetitle" => "Modifier categorie",
+                                "cheminVueBody" => "categorie/create.php",
+                            ]);
+                            exit(1);
+                        }
+                    }
+                    $bool = (new CategorieRepository)->update($categorie);
+                    if ($bool) {
+                        MessageFlash::ajouter("success", "Catégorie bien mise à jour");
+                    } else {
+                        MessageFlash::ajouter("warning", "Mise à jour échouée");
+                    }
+                    header("Location: frontController.php?action=readAll&controller=categorie");
+                } else {
+                    MessageFlash::ajouter("warning", "Cette catégorie n'existe pas");
+                    self::afficheVue([
+                        "categorie" => $categorie,
+                        "action" => "update",
+                        "pagetitle" => "Modifier categorie",
+                        "cheminVueBody" => "categorie/create.php",
+                    ]);
+                    exit(1);
+                }
+            } else {
+                MessageFlash::ajouter("danger", "Nom non renseignée !!");
+                header("Location: frontController.php?action=readAll&controller=categorie");
+            }
+        } else {
+            MessageFlash::ajouter("danger", "Vous n'etes pas Administrateur !");
+            header("Location: frontController.php");
+        }
+    }
+
+    public static function created()
+    {
+        if (ConnexionUtilisateur::estAdministrateur()) {
+            if (isset($_REQUEST['nom']) && isset($_REQUEST['description']) && !empty($_FILES['file-upload']) && is_uploaded_file($_FILES['file-upload']['tmp_name'])) {
+                $categorie = new Categorie($_REQUEST['nom'], $_REQUEST['description'], "");
+                $pic_path = __DIR__ . "/../../assets/images/categories/" . $_FILES['file-upload']['name'];
+                $extension = explode('.', $_FILES['file-upload']['name']);
+                if (in_array(end($extension), ["jpg", "jpeg", "png"]) && $_FILES['file-upload']['size'] < 10 ** 7) { // 10 Mo
+                    if (move_uploaded_file($_FILES['file-upload']['tmp_name'], $pic_path)) {
+                        $_REQUEST['imgPath'] = $_FILES['file-upload']['name'];
+                        $bool = (new CategorieRepository)->save(Categorie::construireDepuisFormulaire($_REQUEST));
+                        if ($bool) {
+                            MessageFlash::ajouter("success", "Catégorie bien crée");
+                            header("Location: frontController.php?action=readAll&controller=categorie");
+                        } else {
+                            MessageFlash::ajouter("warning", "Nom déja existant");
+                            self::afficheVue([
+                                "categorie" => $categorie,
+                                "action" => "create",
+                                "pagetitle" => "Créer Categorie",
+                                "cheminVueBody" => "categorie/create.php",
+                            ]);
+                        }
+                    } else {
+                        MessageFlash::ajouter("warning", "Importation de l'image échouée");
+                        self::afficheVue([
+                            "categorie" => $categorie,
+                            "action" => "create",
+                            "pagetitle" => "Créer Categorie",
+                            "cheminVueBody" => "categorie/create.php",
+                        ]);
+                    }
+                } else {
+                    MessageFlash::ajouter("warning", "Mauvaise extension ou taille de fichier");
+                    self::afficheVue([
+                        "categorie" => $categorie,
+                        "action" => "create",
+                        "pagetitle" => "Créer Categorie",
+                        "cheminVueBody" => "categorie/create.php",
+                    ]);
+                }
+            } else {
+                MessageFlash::ajouter("danger", "Nom non renseigné !!");
+                header("Location: frontController.php?action=readAll&controller=categorie");
+            }
+        } else {
+            MessageFlash::ajouter("danger", "Vous n'etes pas Administrateur !");
+            header("Location: frontController.php");
+        }
     }
 }
---
+++ src/View/categorie/create.php
@@ -0,0 +1,58 @@
+<div class="mt-10 sm:mt-0">
+    <div class="mt-5 px-10 md:col-span-2 md:mt-0">
+        <form enctype="multipart/form-data"
+              action="frontController.php?action=<?php echo $action == "create" ? "created" : "updated" ?>&controller=categorie"
+              method="post">
+            <div class="overflow-hidden shadow sm:rounded-md">
+                <div class=" flex flex-col gap-6 bg-white px-4 py-5 sm:p-6">
+                    <div class="grid grid-cols-6 gap-6">
+                        <div class="col-span-6 sm:col-span-3">
+                            <label for="libelle" class="block text-sm font-medium text-gray-700">Nom Catégorie</label>
+                            <input type="text" name="nom" id="libelle" <?php echo $action == "create" ? "required" : "readonly" ?>
+                                <?php echo isset($categorie) ? "value='" . htmlspecialchars($categorie->getNom()) . "'" : "placeholder='ex: Péripherique externe'" ?>
+                                   class="mt-1 block h-7 w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
+                        </div>
+                    </div>
+
+                    <div class="flex flex-col gap-6">
+                        <div>
+                            <label for="about" class="block text-sm font-medium text-gray-700">Description</label>
+                            <div class="mt-1">
+                                <input id="about" name="description"
+                                    <?php echo isset($categorie) ? "value='" . htmlspecialchars($categorie->getDescription()) . "'" : "placeholder='ex: Tous type de peripherique comme des casques, micro ou webcam ...'" ?>
+                                       class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" />
+                            </div>
+                        </div>
+                        <label class="block text-sm font-medium text-gray-700">Image Article</label>
+                        <div class="mt-1 flex justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6">
+                            <div class="space-y-1 text-center">
+                                <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none"
+                                     viewBox="0 0 48 48" aria-hidden="true">
+                                    <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                                          stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                </svg>
+                                <div class="flex text-sm text-gray-600">
+                                    <label for="file-upload"
+                                           class="relative cursor-pointer rounded-md bg-white font-medium text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:text-indigo-500">
+                                        <span>Upload a file</span>
+                                        <input id="file-upload" name="file-upload" type="file"
+                                               class="sr-only" <?php echo $action == "create" ? "required" : "" ?>>
+                                    </label>
+                                    <p class="pl-1">or drag and drop</p>
+                                </div>
+                                <p class="text-xs text-gray-500">PNG, JPG, JPEG up to 10MB</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
+                    <button type="submit"
+                            class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                        Save
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+
