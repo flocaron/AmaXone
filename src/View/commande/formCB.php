b0dbc3a7a4cc3aa8be5e4da5e3fc7ea525e8c3b9
flocaron <floflocaron27@gmail.com>
2023-01-05 00:46:41 +0100

    format + move checked

--- src/View/commande/formCB.php
+++ src/View/commande/formCB.php
@@ -92,7 +92,7 @@
             <div class="mb-3 flex -mx-2">
                 <div class="px-2">
                     <label for="type1" class="flex items-center cursor-pointer">
-                        <input id="type1" name="type" type="radio" class="form-radio h-5 w-5 text-indigo-500">
+                        <input checked id="type1" name="type" type="radio" class="form-radio h-5 w-5 text-indigo-500">
                         <img src="https://leadershipmemphis.org/wp-content/uploads/2020/08/780370.png"
                              class="h-8 ml-3" alt="...">
                     </label>
@@ -100,7 +100,7 @@
                 <div class="px-2">
                     <label for="type2" class="flex items-center cursor-pointer">
                         <input type="radio" class="form-radio h-5 w-5 text-indigo-500" name="type" id="type2"
-                               checked>
+                        >
                         <img src="https://www.sketchappsources.com/resources/source-image/PayPalCard.png"
                              class="h-8 ml-3" alt="...">
                     </label>
@@ -109,15 +109,17 @@
             <div class="mb-3">
                 <label for="nom_id" class="font-bold text-sm mb-2 ml-1">Nom du titulaire</label>
                 <div>
-                    <input id="nom_id" class="w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors"
+                    <input id="nom_id"
+                           class="w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors"
                            placeholder="John Smith" type="text" name="nom"
-                           value="<?= htmlspecialchars($val['nom']) ?>" required />
+                           value="<?= htmlspecialchars($val['nom']) ?>" required/>
                 </div>
             </div>
             <div class="mb-3">
                 <label for="num_id" class="font-bold text-sm mb-2 ml-1">Numéro de la carte</label>
                 <div>
-                    <input id="num_id" class="w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors"
+                    <input id="num_id"
+                           class="w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors"
                            placeholder="0000 0000 0000 0000" type="number" name="num"
                            value="<?= htmlspecialchars($val['num']) ?>" required/>
                 </div>
@@ -126,7 +128,8 @@
                 <div class="px-2 w-1/2">
                     <label for="mois_id" class="font-bold text-sm mb-2 ml-1">Date d'expiration</label>
                     <div>
-                        <select id="mois_id" name="mois" class="form-select w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors cursor-pointer">
+                        <select id="mois_id" name="mois"
+                                class="form-select w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors cursor-pointer">
                             <option value="01" <?= $val['mois'] == "1" ? "selected" : "" ?> >01 - Janvier</option>
                             <option value="02" <?= $val['mois'] == "2" ? "selected" : "" ?> >02 - Fevrier</option>
                             <option value="03" <?= $val['mois'] == "3" ? "selected" : "" ?> >03 - Mars</option>
@@ -143,26 +146,29 @@
                     </div>
                 </div>
                 <div class="px-2 w-1/2">
-                    <select id="mois_id" name="an" class="form-select w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors cursor-pointer">
-                        <option value="2023" <?= $val['an'] == "2023" ? "selected" : ""?> >2023</option>
-                        <option value="2024" <?= $val['an'] == "2024" ? "selected" : ""?> >2024</option>
-                        <option value="2025" <?= $val['an'] == "2025" ? "selected" : ""?> >2025</option>
-                        <option value="2026" <?= $val['an'] == "2026" ? "selected" : ""?> >2026</option>
-                        <option value="2027" <?= $val['an'] == "2027" ? "selected" : ""?> >2027</option>
-                        <option value="2028" <?= $val['an'] == "2028" ? "selected" : ""?> >2028</option>
-                        <option value="2029" <?= $val['an'] == "2029" ? "selected" : ""?> >2029</option>
+                    <select id="mois_id" name="an"
+                            class="form-select w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors cursor-pointer">
+                        <option value="2023" <?= $val['an'] == "2023" ? "selected" : "" ?> >2023</option>
+                        <option value="2024" <?= $val['an'] == "2024" ? "selected" : "" ?> >2024</option>
+                        <option value="2025" <?= $val['an'] == "2025" ? "selected" : "" ?> >2025</option>
+                        <option value="2026" <?= $val['an'] == "2026" ? "selected" : "" ?> >2026</option>
+                        <option value="2027" <?= $val['an'] == "2027" ? "selected" : "" ?> >2027</option>
+                        <option value="2028" <?= $val['an'] == "2028" ? "selected" : "" ?> >2028</option>
+                        <option value="2029" <?= $val['an'] == "2029" ? "selected" : "" ?> >2029</option>
                     </select>
                 </div>
             </div>
             <div class="mb-10">
                 <label for="crypto_id" class="font-bold text-sm mb-2 ml-1">Security code</label>
                 <div>
-                    <input id="crypto_id" class="w-32 px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors"
-                           name="crypto" placeholder="000" type="text" required />
+                    <input id="crypto_id"
+                           class="w-32 px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors"
+                           name="crypto" placeholder="000" type="text" required/>
                 </div>
             </div>
             <div>
-                <button type="submit" class="block w-full max-w-xs mx-auto bg-indigo-500 hover:bg-indigo-700 focus:bg-indigo-700 text-white rounded-lg px-3 py-3 font-semibold">
+                <button type="submit"
+                        class="block w-full max-w-xs mx-auto bg-indigo-500 hover:bg-indigo-700 focus:bg-indigo-700 text-white rounded-lg px-3 py-3 font-semibold">
                     <i class="mdi mdi-lock-outline mr-1"></i> PAYER <?= $total ?> €
                 </button>
                 <input type="hidden" name="action" value="validerCommande"/>
