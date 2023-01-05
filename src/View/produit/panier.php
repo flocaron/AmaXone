1319f44902a19a708939b7042be8369691368a5e
Dimitri Copley <73848767+Bergenheiser@users.noreply.github.com>
2023-01-05 10:43:33 +0100

    Quantity Select Forms

--- src/View/produit/panier.php
+++ src/View/produit/panier.php
@@ -55,93 +55,106 @@
 
                                             <?php
                                             $total = 0;
-                                            foreach ($panierProduit as $produitSerialize => $qte) {
-                                                $produit = unserialize($produitSerialize);
+                                            foreach ($panierProduit
 
-                                                ?>
+                                            as $produitSerialize => $qte) {
+                                            $produit = unserialize($produitSerialize);
 
-                                                <li class="flex py-6">
-                                                    <div class="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
-                                                        <img src="<?php echo "../assets/images/produits/" . htmlspecialchars($produit->getImgPath()); ?>"
-                                                             alt="Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch."
-                                                             class="h-full w-full object-cover object-center">
-                                                    </div>
+                                            ?>
 
-                                                    <div class="ml-4 flex flex-1 flex-col">
-                                                        <div>
-                                                            <div class="flex justify-between text-base font-medium text-gray-900">
-                                                                <h3>
-                                                                    <a href="#"><?php echo htmlspecialchars($produit->getLibelle()) ?> </a>
-                                                                </h3>
-                                                                <p class="ml-4"><?php echo htmlspecialchars($produit->getPrix()) ?>
-                                                                    €/p.</p>
-                                                            </div>
-                                                        </div>
-                                                        <div class="flex flex-1 items-end justify-between text-sm">
-                                                            <p class="text-gray-500">x <?php echo $qte; ?></p>
+                                            <li class="flex py-6">
+                                                <div class="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+                                                    <img src="<?php echo "../assets/images/produits/" . htmlspecialchars($produit->getImgPath()); ?>"
+                                                         alt="Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch."
+                                                         class="h-full w-full object-cover object-center">
+                                                </div>
 
-                                                            <div class="flex">
-                                                                <a href="frontController.php?action=addPanier&controller=produit&id=<?php echo htmlspecialchars($produit->getId()) ?>">
-                                                                    <button type="button"
-                                                                            class="font-medium text-indigo-600 hover:text-indigo-500">
-
-                                                                        Ajouter
-                                                                    </button>
-                                                                </a>
-                                                            </div>
-
-                                                            <div class="flex">
-
-                                                                <a href="frontController.php?action=removePanier&controller=produit&id=<?php echo htmlspecialchars($produit->getId()) ?>">
-                                                                    <button type="button"
-                                                                            class="font-medium text-indigo-600 hover:text-indigo-500">
-
-                                                                        Retirer
-                                                                    </button>
-                                                                </a>
-
-                                                            </div>
+                                                <div class="ml-4 flex flex-1 flex-col">
+                                                    <div>
+                                                        <div class="flex justify-between text-base font-medium text-gray-900">
+                                                            <h3>
+                                                                <a href="#"><?php echo htmlspecialchars($produit->getLibelle()) ?> </a>
+                                                            </h3>
+                                                            <p class="ml-4"><?php echo htmlspecialchars($produit->getPrix()) ?>
+                                                                €/p.</p>
                                                         </div>
                                                     </div>
-                                                </li>
-                                                <?php
-                                                $total += $produit->getPrix() * $qte;
-                                            } ?>
-                                        </ul>
+                                                    <div class="flex flex-1 items-end justify-between text-sm">
+                                                        <p class="text-gray-500">x <?php echo $qte; ?></p>
+                                                        <form action="frontController.php" method="post"
+                                                              id="QteForm<?php echo $produit->getId(); ?>">
+                                                            <input type="hidden" name="action" value="addAllPanier">
+                                                            <input type="hidden" name="controller" value="produit">
+                                                            <input type="hidden" name="id"
+                                                                   value="<?= htmlspecialchars($produit->getId()); ?>">
+                                                            <div class="flex">
+                                                                <select name="qte"
+                                                                        onchange="let QteForm = document.getElementById('QteForm<?php echo htmlspecialchars($produit->getId());?>'); QteForm.submit();">
+                                                                    <?php
+                                                                    for ($i = 1; $i <= 10; $i++) {
+                                                                        if ($i == $qte) {
+                                                                            echo "<option value='$i' selected>$i</option>";
+                                                                        } else {
+                                                                            echo "<option value='$i'>$i</option>";
+                                                                        }
+                                                                    }
+                                                                    ?>
+                                                                </select>
+                                                        </form>
+                                                    </div>
+
+                                                    <div class="flex">
+
+                                                        <a href="frontController.php?action=removePanier&controller=produit&id=<?php echo htmlspecialchars($produit->getId()) ?>">
+                                                            <button type="button"
+                                                                    class="font-medium text-indigo-600 hover:text-indigo-500">
+
+                                                                Retirer
+                                                            </button>
+                                                        </a>
+
+                                                    </div>
+                                                </div>
                                     </div>
-                                </div>
-                            </div>
-
-                            <div class="border-t border-gray-200 py-6 px-4 sm:px-6">
-                                <div class="flex justify-between text-base font-medium text-gray-900">
-                                    <p>Sous-Total : </p>
-                                    <p><?php echo $total; ?>€</p>
-                                </div>
-                                <p class="mt-0.5 text-sm text-gray-500">Frais de ports et taxes incluses.</p>
-                                <div class="mt-6">
-                                    <a href="frontController.php?action=passerCommande&controller=commande"
-                                       class="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700">Commander</a>
-                                </div>
-                                <div class="mt-6 flex justify-center text-center text-sm text-gray-500">
-                                    <p>
-                                        ou
-                                        <a href="frontController.php?action=catalogue&controller=categorie">
-                                            <button type="button"
-                                                    class="font-medium text-indigo-600 hover:text-indigo-500">
-                                                Continuer d'explorer
-                                                <span aria-hidden="true"> &rarr;</span>
-                                            </button>
-                                        </a>
-                                    </p>
+                                    </li>
+                                    <?php
+                                    $total += $produit->getPrix() * $qte;
+                                    } ?>
+                                    </ul>
                                 </div>
                             </div>
                         </div>
+
+                        <div class="border-t border-gray-200 py-6 px-4 sm:px-6">
+                            <div class="flex justify-between text-base font-medium text-gray-900">
+                                <p>Sous-Total : </p>
+                                <p><?php echo $total; ?>€</p>
+                            </div>
+                            <p class="mt-0.5 text-sm text-gray-500">Frais de ports et taxes incluses.</p>
+                            <div class="mt-6">
+                                <a href="frontController.php?action=passerCommande&controller=commande"
+                                   class="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700">Commander</a>
+                            </div>
+                            <div class="mt-6 flex justify-center text-center text-sm text-gray-500">
+                                <p>
+                                    ou
+                                    <a href="frontController.php?action=catalogue&controller=categorie">
+                                        <button type="button"
+                                                class="font-medium text-indigo-600 hover:text-indigo-500">
+                                            Continuer d'explorer
+                                            <span aria-hidden="true"> &rarr;</span>
+                                        </button>
+                                    </a>
+                                </p>
+                            </div>
+                        </div>
                     </div>
-
-
                 </div>
+
+
             </div>
         </div>
     </div>
+</div>
 </div>
 
